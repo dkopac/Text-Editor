ae43a5fff89d7ad5fbc809e84e47e474517365b3
[Dan <123705636+dkopac@users.noreply.github.com>
2023-07-01 10:35:52 -0400

    client side set up

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => 
+{
+  console.error('PUT to the database');
+  // Create connection to the database
+  const jateDb = await openDB('jate', 1);
+  // Create a transaction on the database
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // Create a reference to the object store
+  const store = tx.objectStore('jate');
+  // Use .put() to add the content to the database
+  const request = store.put({ id: 1, value: content });
+  //Confirm the request was successful
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => 
+{
+  console.error('GET from the database');
+  // Create connection to the database
+  const jateDb = await openDB('jate', 1);
+  // Create a transaction on the database
+  const tx = jateDb.transaction('jate', 'readonly');
+  // Create a reference to the object store
+  const store = tx.objectStore('jate');
+  // Use .get() to retrieve the content from the database
+  const request = store.get(1);
+  //Confirm the request was successful
+  const result = await request;
+  console.log('Data retrieved from the database', result);
+  return result;
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,55 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin to generate html file and inject our bundles
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Just Another Text Editor',
+      }),
+
+      // Inject Custom Service Worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js',
+      }),
+
+      // Webpack plugin to generate a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor',
+        background_color: '#ffffff',
+        theme_color: '#2196f3',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },   
     ],
+      }),
+    ],
+
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test:/\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/env'],
+              plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread'],
+            },
+          }
+        }, 
       ],
     },
   };
